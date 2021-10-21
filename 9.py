707f0e96717ef419ccb1d0b7c3a660524cd7e6e0
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-10-21 18:37:41 +0300

    add ninth task

--- 9.py
+++ 9.py
@@ -1,12 +1,11 @@
-digits = input('Введите натуральные числа через пробел - ').split()
+digits = input('Введите натуральные числа через пробел - ').strip().split()
 max = 0
-summ = 0
 print(digits)
 
 for i in digits:
+    summ = 0
     for j in i:
         summ += int(j)
-    print(summ)
     if summ > max:
         max = summ
         digit = i

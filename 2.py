de306ada0c41a32cdac0877ddfd1b2905b81d045
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-10-24 19:09:38 +0300

    add third task

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 import random
 
-numbers = [random.randint(1, 100) for i in range(20)]
+digits = [random.randint(1, 100) for i in range(20)]
 list_index = []
-for i, j in enumerate(numbers, 0):
-    if not j % 2:
-        list_index.append(i)
+for index, digit in enumerate(digits, 0):
+    if not digit % 2:
+        list_index.append(index)
 
 print(f'Indexes: {list_index}')
-print(f'Array: {numbers}')
+print(f'Array: {digits}')

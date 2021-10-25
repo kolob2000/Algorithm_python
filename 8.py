c65c74525d5bf6a830594eeecae4824795fc6b45
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-10-25 17:37:30 +0300

    edit eighth task

--- 8.py
+++ 8.py
@@ -1,5 +1,5 @@
 import random
-
+# ############# Первый вариант
 matrix = []
 for _ in range(4):
     tmp = []
@@ -12,5 +12,25 @@ for _ in range(4):
             tmp.append(j)
             summ += j
     matrix.append(tmp)
+print('Первый вариант:')
+for i in matrix:
+    print(i)
+################### Второй вариант
+matrix = [[random.randint(1, 10) for _ in range(4)] for _ in range(4)]
+for i in matrix:
+    summ = 0
+    for j in i:
+        summ += j
+    i.append(summ)
+
+print('\n Второй вариант:')
+for i in matrix:
+    print(i)
+
+############### Тритий вариант
+matrix = [[random.randint(1, 10) for _ in range(4)] for _ in range(4)]
+for i in matrix:
+    i.append(sum(i))
+print('\n Третий вариант:')
 for i in matrix:
     print(i)
